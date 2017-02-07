038978bed1c08211bf884834ac80dda748adbbb6
ITLAB <ITLAB@IT>
2017-02-07 14:26:18 +0530

    8th commit

--- src/com/example/basicgraphics/MainActivity.java
+++ src/com/example/basicgraphics/MainActivity.java
@@ -10,49 +10,55 @@ import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
- 
+
 	BView shapes;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		shapes = new BView(this);
 		setContentView(shapes);
-		
+
 	}
-		private class BView extends View {
-			public BView(Context mcontext) {
-				super(mcontext);
-	
-			}	
-			@Override
-			protected void onDraw(Canvas canvas) {
-				super.onDraw(canvas);
-				
-				Paint paint = new Paint();
-				paint.setStyle(Paint.Style.FILL);
-				
-				paint.setColor(Color.RED);
-				canvas.drawPaint(paint);
-				
-				paint.setAntiAlias(false);
-				paint.setColor(Color.BLUE);
-				canvas.drawCircle(25, 25, 20, paint);
-				
-				paint.setAntiAlias(true);
-				paint.setColor(Color.BLACK);
-				canvas.drawCircle(60, 25, 20, paint);
-				
-				paint.setAntiAlias(false);
-				paint.setColor(Color.GRAY);
-				canvas.drawRect(100, 5, 200, 30, paint);
-				
-				canvas.rotate(-40);
-				
-				paint.setStyle(Paint.Style.FILL);
-				canvas.drawText("Graphics Rotation", 45, 185, paint);
-	 
-				// undo the rotate
-				canvas.restore();
-			}
-		}	
+
+	private class BView extends View {
+		public BView(Context mcontext) {
+			super(mcontext);
+
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+
+			paint.setColor(Color.RED);
+			canvas.drawPaint(paint);
+
+			paint.setAntiAlias(false);
+			paint.setColor(Color.BLUE);
+			canvas.drawCircle(25, 25, 20, paint);
+
+			paint.setAntiAlias(true);
+			paint.setColor(Color.BLACK);
+			canvas.drawCircle(80, 50, 30, paint);
+
+			paint.setAntiAlias(false);
+			paint.setColor(Color.GRAY);
+			canvas.drawRect(135, 75, 200, 200, paint);
+
+			canvas.rotate(-40);
+
+			canvas.restore();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
